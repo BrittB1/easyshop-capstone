51a11c637c68dd196aaa7be0686925e70ede725a
Brittany Buchanan <bbuchanan0400@gmail.com>
2024-12-19 14:42:55 -0500

    Implemented getById and create methods for MySQLCategoryDao

--- easyshop-backend/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ easyshop-backend/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -5,18 +5,15 @@ import org.yearup.data.CategoryDao;
 import org.yearup.models.Category;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.yearup.data.mysql.MySqlProductDao.mapRow;
+
 @Component
-public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
-{
-    public MySqlCategoryDao(DataSource dataSource)
-    {
+public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao {
+    public MySqlCategoryDao(DataSource dataSource) {
         super(dataSource);
     }
 
@@ -26,7 +23,7 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
         List<Category> categories = new ArrayList<>();
         String sql = "SELECT * FROM categories";
 
-        try(Connection connection = getConnection()) {
+        try (Connection connection = getConnection()) {
             PreparedStatement statement = connection.prepareStatement(sql);
             ResultSet row = statement.executeQuery();
 
@@ -34,46 +31,73 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
                 Category category = mapRow(row);
                 categories.add(category);
             }
-        }catch (SQLException e) {
-                System.out.println("Error getting categories: " + e.getMessage());
-            }
-        return categories;
+        } catch (SQLException e) {
+            System.out.println("Error getting categories: " + e.getMessage());
         }
+        return categories;
+    }
 
     @Override
-    public Category getById(int categoryId)
-    {
-        // get category by id
+    public Category getById(int categoryId) {
+        String sql = "SELECT * FROM categories WHERE category_id = ?";
+
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, categoryId);
+
+            ResultSet row = statement.executeQuery();
+
+            if (row.next()) {
+                return mapRow(row);
+            }
+            return null;
+        } catch (SQLException e) {
+
+            System.out.println("Could not fine category #" + categoryId + ": " + e.getMessage());
+            return null;
+        }
+    }
+
+    @Override
+    public Category create(Category category) {
+        String sql = "INSERT INTO categories (name, description) VALUES (?, ?)";
+
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            statement.setString(1, category.getName());
+            statement.setString(2, category.getDescription());
+
+            statement.executeUpdate();
+
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                int generatedId = generatedKeys.getInt(1);
+                category.setCategoryId(generatedId);
+            }
+            return category;
+        } catch (SQLException e) {
+            System.out.println("Could not create category: " + e.getMessage());
+        }
         return null;
     }
 
     @Override
-    public Category create(Category category)
-    {
-        // create a new category
-        return null;
-    }
-
-    @Override
-    public void update(int categoryId, Category category)
-    {
+    public void update(int categoryId, Category category) {
         // update category
     }
 
     @Override
-    public void delete(int categoryId)
-    {
+    public void delete(int categoryId) {
         // delete category
     }
 
-    private Category mapRow(ResultSet row) throws SQLException
-    {
+    private Category mapRow(ResultSet row) throws SQLException {
         int categoryId = row.getInt("category_id");
         String name = row.getString("name");
         String description = row.getString("description");
 
-        Category category = new Category()
-        {{
+        Category category = new Category() {{
             setCategoryId(categoryId);
             setName(name);
             setDescription(description);
